207afb66f3f1f2f44e96e1f1e1f4172f6abea131
Tom Burdick <thomas.burdick@intel.com>
2022-07-28 11:34:48 -0500

    revert hello world sample
    Signed-off-by: Tom Burdick <thomas.burdick@intel.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,17 +6,7 @@
 
 #include <zephyr/zephyr.h>
 
-static uint32_t busy = 0;
-void timer_fn(struct k_timer *tm)
-{
-	k_sem_give()
-}
-
-K_TIMER_DEFINE(timer, timer_fn, NULL);
-
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
-	k_sleep(K_MSEC(100));
-	k_timer_start(&timer, K_MSEC(100), K_MSEC(100));
 }
