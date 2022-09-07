cff7dbdc23c6ae76d6509a8959843ff134bed378
Tom Burdick <thomas.burdick@intel.com>
2022-09-07 09:26:36 -0500

    logging: adsp hda backend refinements and additional test
    Additional testing showed than when using printk the logger would start
    sticking and spitting out nulls which is wrong. This seems to have been
    caused by the initial ipc message to read all the dma buffers on the host.
    
    Removing that, the issue seems to have been solved.
    
    Signed-off-by: Tom Burdick <thomas.burdick@intel.com>

--- tests/boards/intel_adsp/hda_log/src/logger.c
+++ tests/boards/intel_adsp/hda_log/src/logger.c
@@ -21,13 +21,14 @@ ZTEST(intel_adsp_hda_log, test_hda_logger)
 	/* Wait a moment so the output isn't mangled */
 	k_msleep(100);
 
-	/* Ensure multiple wraps and many many logs are written */
-	for (int i = 0; i < 4096; i++) {
-		LOG_DBG("test hda log message %d", i);
+	/* Ensure multiple wraps and many many logs are written without delays */
+	for (int i = 0; i < 16384; i++) {
+		if (IS_ENABLED(CONFIG_LOG_PRINTK)) {
+			printk("test hda log message %d\n", i);
+		} else {
+			LOG_INF("test hda log message %d", i);
+		}
 	}
-
-	/* Wait another moment so dma may flush */
-	k_sleep(K_MSEC(100));
 }
 
 ZTEST_SUITE(intel_adsp_hda_log, NULL, NULL, NULL, NULL, NULL);
