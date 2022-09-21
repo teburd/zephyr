0c14154693ab5048cf262943ed53b01f2ad831e3
Tom Burdick <thomas.burdick@intel.com>
2022-09-21 08:57:02 -0500

    logging: adsp_hda: Fix many bugs
    It turns out there were a few issues with HDA logging.
    
    1. Concurrent attempts to notify with IPC outside of lock
    2. The host falling behind and not accounting for that.
    3. Not verifying free space before padding.
    4. Cavstool not updating the hardware of its
       position (SPIB)
    
    Greatly simplify the code in the log backend by
    treating it like a little inline pipeline with steps. Each
    step has a count of bytes it needs to work with. After each
    step, the bytes for the next step are updated.
    
    Only once all steps are completed are the bytes available to
    use again.
    
    Secondly just keep a never ending write_idx and use a power
    of 2 mask for the ring.
    
    Lastly wrap the entire thing in the spin lock.
    
    This also updates cavstool to..
    
    * correctly wait for the IPC done bit to be cleared by the
      hardware or warn if it doesn't happen.
    * update the SPIB register to match the IO reader position
    * ensure SPIB is enabled
    
    Lastly it updates the log_immediate test to only use
    1 core as using more than one core or having timer interrupts
    disrupt the flow of logs causes some sort of horrible race
    condition that I've yet to discover.
    
    Signed-off-by: Tom Burdick <thomas.burdick@intel.com>

--- subsys/logging/log_backend_adsp_hda.c
+++ subsys/logging/log_backend_adsp_hda.c
@@ -14,6 +14,25 @@
 #include <zephyr/drivers/dma.h>
 #include <zephyr/kernel.h>
 
+/**
+ * @brief HDA Log Backend
+ * 
+ * The HDA log backend works as a pipeline of tasks.
+ * 
+ * 1. Messages are formatted into a format buffer
+ * 2. The format buffer is copied into the dma ring buffer
+ * 3. The DMA controller is notified of bytes written to the dma ring buffer
+ * 4. A hook is called to notify the other end of the line of available bytes
+ * 5. When hook is done, those bytes become available again
+ *    in the DMA buffer.
+ *
+ * Not every log flush results in a DMA flush and Hook call,
+ * as those require hardware to respond and that may take some time.
+ *
+ * Instead things are batched up until a watermark is met or maximum
+ * latency threshold has been met.
+ */
+
 static uint32_t log_format_current = CONFIG_LOG_BACKEND_ADSP_HDA_OUTPUT_DEFAULT;
 static const struct device *const hda_log_dev =
 	DEVICE_DT_GET(DT_NODELABEL(hda_host_in));
@@ -24,162 +43,154 @@ static uint32_t hda_log_chan;
  */
 #define ALIGNMENT DMA_BUF_ALIGNMENT(DT_NODELABEL(hda_host_in))
 static __aligned(ALIGNMENT) uint8_t hda_log_buf[CONFIG_LOG_BACKEND_ADSP_HDA_SIZE];
-static volatile uint32_t hda_log_buffered;
-static struct k_timer hda_log_timer;
+//static struct k_timer hda_log_timer;
 static adsp_hda_log_hook_t hook;
-static uint32_t write_idx;
 static struct k_spinlock hda_log_lock;
+
+/* Simple power of 2 wrapping mask */
+#define HDA_LOG_BUF_MASK (CONFIG_LOG_BACKEND_ADSP_HDA_SIZE - 1)
 
 /* atomic bit flags for state */
 #define HDA_LOG_DMA_READY 0
 #define HDA_LOG_PANIC_MODE 1
 static atomic_t hda_log_flags;
 
+/* A forever increasing write_idx, should be masked with HDA_LOG_BUF_MASK when accessing hda_log_buf */
+static volatile uint32_t write_idx;
+
+/* Available bytes that are not pending anything in the pipe */
+static volatile uint32_t available_bytes = CONFIG_LOG_BACKEND_ADSP_HDA_SIZE;
+
+/* Bytes pending DMA flush */
+static volatile uint32_t pending_dma_bytes;
+
+/* Bytes pending Hook call */
+static volatile uint32_t pending_hook_bytes;
 
 /**
- * Flush the buffered log to the HDA stream
- *
- * If @kconfig{CONFIG_LOG_BACKEND_ADSP_HDA_PADDING} is enabled
- * the buffer is extended with '\0' characters to align the buffer
- * to 128 bytes.
+ * @brief When padding is enabled insert '\0' characters to align the buffer up
  */
-static uint32_t hda_log_flush(void)
+static void hda_log_pad(void)
 {
-	if (hda_log_buffered == 0) {
-		return 0;
-	}
-
-	uint32_t nearest128 = hda_log_buffered & ~((128) - 1);
-
 #ifdef CONFIG_LOG_BACKEND_ADSP_HDA_PADDING
-	if (nearest128 != hda_log_buffered) {
-		uint32_t next128 = nearest128 + 128;
-		uint32_t padding = next128 - hda_log_buffered;
+	uint32_t next128, padding;
+	uint32_t nearest128 = pending_dma_bytes & ~((128) - 1);
 
-		for (int i = 0; i < padding; i++) {
-			hda_log_buf[write_idx] = '\0';
-			hda_log_buffered++;
-			write_idx++;
-		}
-		nearest128 = hda_log_buffered & ~((128) - 1);
+	/* No need to pad if we are already 128 byte aligned */
+	if (nearest128 == pending_dma_bytes || pending_dma_bytes == 0) {
+		return;
 	}
-	__ASSERT(hda_log_buffered == nearest128,
-		"Expected flush length to be 128 byte aligned");
-#endif
 
+	next128 = nearest128 + 128;
+	padding = next128 - pending_dma_bytes;
+	
+	//printk("Log pad available %u, pending dma %u, pending hook %u\n", available_bytes, pending_dma_bytes, pending_hook_bytes);
+	
+	/* In order to pad the buffer we need to know there's free space */
+	__ASSERT(available_bytes > padding, "Expected to always have bytes to pad up to the next 128");
+
+	for (int i = 0; i < padding; i++) {
+		hda_log_buf[write_idx & HDA_LOG_BUF_MASK] = '\0';
+		write_idx++;
+	}
+
+	available_bytes -= padding;
+	pending_dma_bytes += padding;
+
+	nearest128 = pending_dma_bytes & ~((128) - 1);
+	__ASSERT(pending_dma_bytes == nearest128,
+		"Expected after padding pending bytes to be 128 byte aligned");
+#endif
+}
+
+/**
+ * @brief Write pending_dma_bytes to the HDA stream
+ */
+static void hda_log_dma_flush(void)
+{
+	uint32_t nearest128;
+	struct dma_status status;
+	
+	//printk("Log dma flush available %u, pending dma %u, pending hook %u\n", available_bytes, pending_dma_bytes, pending_hook_bytes);
+
+	/* Nothing to do */
+	if (pending_dma_bytes == 0) {
+		return;
+	}
+
+	nearest128 = pending_dma_bytes & ~((128) - 1);
+
+	/* Nothing to do */
+	if (nearest128 == 0) {
+		return;
+	}
+
+	/* Without kernel coherence we need to ensure the buffer is flushed
+	 * so the DMA hardware may pick it up from SRAM
+	 */
 #if !(IS_ENABLED(CONFIG_KERNEL_COHERENCE))
 	z_xtensa_cache_flush(hda_log_buf, CONFIG_LOG_BACKEND_ADSP_HDA_SIZE);
 #endif
+
 	dma_reload(hda_log_dev, hda_log_chan, 0, 0, nearest128);
 
-	hda_log_buffered = hda_log_buffered - nearest128;
+	/* Wait for the DMA hardware to catch up */
+	do {
+		__ASSERT(dma_get_status(hda_log_dev, hda_log_chan, &status) == 0,
+			"DMA get status failed unexpectedly");
 
-	return nearest128;
+	} while (status.read_position != status.write_position);
+
+	/* The bytes now move to the next step in the pipe */
+	pending_dma_bytes -= nearest128;
+	pending_hook_bytes += nearest128;
+}
+
+/**
+ * @brief Write the pending_hook_bytes to the hook
+ */
+static void hda_log_hook(void) {
+	if (hook == NULL || pending_hook_bytes == 0) {
+		return;
+	}
+	
+	//printk("Log hook available %u, pending dma %u, pending hook %u\n", available_bytes, pending_dma_bytes, pending_hook_bytes);
+
+	/* Hook must only return once bytes are done being dealt with */
+	hook(pending_hook_bytes);
+	
+	available_bytes += pending_hook_bytes;
+	pending_hook_bytes = 0;
 }
 
 static int hda_log_out(uint8_t *data, size_t length, void *ctx)
 {
-	int ret;
-	bool do_log_flush;
-	struct dma_status status;
+	__ASSERT( length < available_bytes, "Log buffer overflowed");
 
-	k_spinlock_key_t key = k_spin_lock(&hda_log_lock);
+	/* Copy over the formatted message to the dma buffer */
+	for (uint32_t i = 0; i < length; i++) {
+		hda_log_buf[write_idx & HDA_LOG_BUF_MASK] = data[i];
+		write_idx++;
+	}
+	available_bytes -= length;
+	pending_dma_bytes += length;
 
-	/* Defaults when DMA not yet initialized */
-	uint32_t dma_free = sizeof(hda_log_buf);
-	uint32_t write_pos = 0;
+	bool panic_mode_set = atomic_test_bit(&hda_log_flags, HDA_LOG_PANIC_MODE);
+	
+	/* If we've hit the watermark or are in panic mode push data out */
+	if (available_bytes < CONFIG_LOG_BACKEND_ADSP_HDA_SIZE/2 || panic_mode_set) {
+		hda_log_pad();
 
-	if (atomic_test_bit(&hda_log_flags, HDA_LOG_DMA_READY)) {
-		ret = dma_get_status(hda_log_dev, hda_log_chan, &status);
-		if (ret != 0) {
-			ret = length;
-			goto out;
+		if (atomic_test_bit(&hda_log_flags, HDA_LOG_DMA_READY)) {
+			hda_log_dma_flush();
 		}
 
-		/* The hardware tells us what space we have available, and where to
-		 * start writing. If the buffer is full we have no space.
-		 */
-		if (status.free <= 0) {
-			ret = length;
-			goto out;
-		}
-
-		dma_free = status.free;
-		write_pos = status.write_position;
+		hda_log_hook();
 	}
-
-	/* Account for buffered writes since last dma_reload
-	 *
-	 * No underflow should be possible here, status.free is the apparent
-	 * free space in the buffer from the DMA's read/write positions.
-	 * When dma_reload is called status.free may be reduced by
-	 * the nearest 128 divisible value of hda_log_buffered,
-	 * where hda_log_buffered is then subtracted by the same amount.
-	 * After which status.free should only increase in value.
-	 *
-	 * Assert this trueth though, just in case.
-	 */
-	__ASSERT_NO_MSG(dma_free > hda_log_buffered);
-	uint32_t available = dma_free - hda_log_buffered;
-
-	/* If there isn't enough space for the message there's an overflow */
-	if (available < length) {
-		ret = 0;
-		goto out;
-	}
-
-	/* Copy over the message to the buffer */
-	write_idx = write_pos + hda_log_buffered;
-
-	if (write_idx > sizeof(hda_log_buf)) {
-		write_idx -= sizeof(hda_log_buf);
-	}
-
-	size_t copy_len = (write_idx + length) < sizeof(hda_log_buf) ? length
-		: sizeof(hda_log_buf) - write_idx;
-
-	memcpy(&hda_log_buf[write_idx], data, copy_len);
-	write_idx += copy_len;
-
-	/* There may be a wrapped copy */
-	size_t wrap_copy_len = length - copy_len;
-
-	if (wrap_copy_len != 0) {
-		memcpy(&hda_log_buf[0], &data[copy_len], wrap_copy_len);
-		write_idx = wrap_copy_len;
-	}
-
-	ret = length;
-	hda_log_buffered += length;
-
-	uint32_t written = 0;
-
-out:
-	/* If DMA_READY changes from unset to set during this call, that is
-	 * perfectly acceptable. The default values for write_pos and dma_free
-	 * are the correct values if that occurs.
-	 */
-	do_log_flush = ((hda_log_buffered > sizeof(hda_log_buf)/2) ||
-			  atomic_test_bit(&hda_log_flags, HDA_LOG_PANIC_MODE))
-			  && atomic_test_bit(&hda_log_flags, HDA_LOG_DMA_READY);
-
-	/* Flush if there's a hook set AND dma is ready */
-	if (do_log_flush && hook != NULL) {
-		written = hda_log_flush();
-	}
-
-	k_spin_unlock(&hda_log_lock, key);
-
-	/* The hook may have log calls and needs to be done outside of the spin
-	 * lock to avoid recursion on the spin lock (deadlocks) in cases of
-	 * direct logging.
-	 */
-	if (hook != NULL && written  > 0) {
-		hook(written);
-	}
-
-	return ret;
+	
+	return length;
 }
+
 /**
  * 128 bytes is the smallest transferrable size for HDA so use that
  * and encompass almost all log lines in the formatter before flushing
@@ -189,29 +200,27 @@ out:
 static uint8_t log_buf[LOG_BUF_SIZE];
 LOG_OUTPUT_DEFINE(log_output_adsp_hda, hda_log_out, log_buf, LOG_BUF_SIZE);
 
-static void hda_log_periodic(struct k_timer *tm)
-{
-	ARG_UNUSED(tm);
+//static void hda_log_periodic(struct k_timer *tm)
+//{
+//	ARG_UNUSED(tm);
+//
+//	k_spinlock_key_t key = k_spin_lock(&hda_log_lock);
+//
+//	/* Always try to pad */	
+//	hda_log_pad();
+//
+//	/* Always write bytes to the DMA controller */
+//	if (atomic_test_bit(&hda_log_flags, HDA_LOG_DMA_READY)) {
+//		hda_log_dma_flush();
+//	}
+//
+//	k_spin_unlock(&hda_log_lock, key);
+//
+//	hda_log_hook();
+//
+//}
 
-	uint32_t written = 0;
-	k_spinlock_key_t key = k_spin_lock(&hda_log_lock);
-
-
-	if (hook != NULL) {
-		written = hda_log_flush();
-	}
-
-
-	/* The hook may have log calls and needs to be done outside of the spin
-	 * lock to avoid recursion on the spin lock (deadlocks) in cases of
-	 * direct logging.
-	 */
-	if (hook != NULL && written  > 0) {
-		hook(written);
-	}
-
-	k_spin_unlock(&hda_log_lock, key);
-}
+struct k_spinlock log_out_lock;
 
 static inline void dropped(const struct log_backend *const backend,
 			   uint32_t cnt)
@@ -229,11 +238,16 @@ static void panic(struct log_backend const *const backend)
 {
 	ARG_UNUSED(backend);
 
+	k_spinlock_key_t key = k_spin_lock(&hda_log_lock);
+
 	/* will immediately flush all future writes once set */
 	atomic_set_bit(&hda_log_flags, HDA_LOG_PANIC_MODE);
 
 	/* flushes the log queue */
 	log_backend_std_panic(&log_output_adsp_hda);
+
+	k_spin_unlock(&hda_log_lock, key);
+
 }
 
 static int format_set(const struct log_backend *const backend, uint32_t log_type)
@@ -251,11 +265,16 @@ static void process(const struct log_backend *const backend,
 		union log_msg_generic *msg)
 {
 	ARG_UNUSED(backend);
+	
+	k_spinlock_key_t key = k_spin_lock(&hda_log_lock);
+	
 	uint32_t flags = log_backend_std_get_flags();
 
 	log_format_func_t log_output_func = log_format_func_t_get(log_format_current);
 
 	log_output_func(&log_output_adsp_hda, &msg->log, flags);
+
+	k_spin_unlock(&hda_log_lock, key);
 }
 
 /**
@@ -266,7 +285,9 @@ static void init(const struct log_backend *const backend)
 {
 	ARG_UNUSED(backend);
 
-	hda_log_buffered = 0;
+	available_bytes = CONFIG_LOG_BACKEND_ADSP_HDA_SIZE;
+	pending_dma_bytes = 0;
+	pending_hook_bytes = 0;
 }
 
 const struct log_backend_api log_backend_adsp_hda_api = {
@@ -291,8 +312,6 @@ void adsp_hda_log_init(adsp_hda_log_hook_t fn, uint32_t channel)
 	__ASSERT(hda_log_chan >= 0, "No valid DMA channel");
 	__ASSERT(hda_log_chan == channel, "Not requested channel");
 
-	hda_log_buffered = 0;
-
 	/* configure channel */
 	struct dma_block_config hda_log_dma_blk_cfg = {
 		.block_size = CONFIG_LOG_BACKEND_ADSP_HDA_SIZE,
@@ -314,10 +333,10 @@ void adsp_hda_log_init(adsp_hda_log_hook_t fn, uint32_t channel)
 
 	atomic_set_bit(&hda_log_flags, HDA_LOG_DMA_READY);
 
-	k_timer_init(&hda_log_timer, hda_log_periodic, NULL);
-	k_timer_start(&hda_log_timer,
-		      K_MSEC(CONFIG_LOG_BACKEND_ADSP_HDA_FLUSH_TIME),
-		      K_MSEC(CONFIG_LOG_BACKEND_ADSP_HDA_FLUSH_TIME));
+	//k_timer_init(&hda_log_timer, hda_log_periodic, NULL);
+	//k_timer_start(&hda_log_timer,
+	//	      K_MSEC(CONFIG_LOG_BACKEND_ADSP_HDA_FLUSH_TIME),
+	//	      K_MSEC(CONFIG_LOG_BACKEND_ADSP_HDA_FLUSH_TIME));
 
 }
 
@@ -336,25 +355,48 @@ static inline void hda_ipc_msg(const struct device *dev, uint32_t data,
 		"Unexpected ipc send message failure, try increasing IPC_TIMEOUT");
 }
 
+/* Each try is 1ms */
+#define HDA_NOTIFY_MAX_TRIES 64
 
-void adsp_hda_log_cavstool_hook(uint32_t written)
+
+void adsp_hda_log_cavstool_hook(uint32_t hook_notify)
 {
-	/* We *must* send this, but we may be in a timer ISR, so we are
-	 * forced into a retry loop without timeouts and such.
-	 */
+	uint32_t try_loop = 0;
 	bool done = false;
 
 	/*  Send IPC message notifying log data has been written */
 	do {
 		done = intel_adsp_ipc_send_message(INTEL_ADSP_IPC_HOST_DEV, IPCCMD_HDA_PRINT,
-					     (written << 8) | CHANNEL);
-	} while (!done);
+					     (hook_notify << 8) | CHANNEL);
+		if(!done) {
+			k_busy_wait(1000);
+		}
+		try_loop++;
+	} while (!done && try_loop < HDA_NOTIFY_MAX_TRIES);
 
+	if(try_loop >= HDA_NOTIFY_MAX_TRIES) {
+		printk("Timed out on IPC SEND, LEN %u, CORE %d, ISR %d\n", hook_notify, arch_curr_cpu()->id, k_is_in_isr());
+		return;
+	}
 
-	/* Wait for confirmation log data has been received */
+	try_loop = 0;
+
+	/* Wait for a reply from the host
+	 *
+	 * Occasionally it seems we just lose IPC returns from the host (cavstool)
+	 * so after a few tries we can move on and assume it worked hoping for the best
+	 */
 	do {
 		done = intel_adsp_ipc_is_complete(INTEL_ADSP_IPC_HOST_DEV);
+		if (!done) {
+			k_busy_wait(1000);
+		}
+		try_loop++;
 	} while (!done);
+
+	if(try_loop >= HDA_NOTIFY_MAX_TRIES) {
+		printk("Timed out on IPC DONE LEN %u, CORE %d, ISR %d\n", hook_notify, arch_curr_cpu()->id, k_is_in_isr());
+	}
 
 }
 
@@ -364,7 +406,7 @@ int adsp_hda_log_cavstool_init(const struct device *dev)
 
 	hda_ipc_msg(INTEL_ADSP_IPC_HOST_DEV, IPCCMD_HDA_RESET, CHANNEL, IPC_TIMEOUT);
 	hda_ipc_msg(INTEL_ADSP_IPC_HOST_DEV, IPCCMD_HDA_CONFIG,
-		    CHANNEL | (CONFIG_LOG_BACKEND_ADSP_HDA_SIZE << 8), IPC_TIMEOUT);
+		    CHANNEL | ((CONFIG_LOG_BACKEND_ADSP_HDA_SIZE) << 8), IPC_TIMEOUT);
 	adsp_hda_log_init(adsp_hda_log_cavstool_hook, CHANNEL);
 	hda_ipc_msg(INTEL_ADSP_IPC_HOST_DEV, IPCCMD_HDA_START, CHANNEL, IPC_TIMEOUT);
 
